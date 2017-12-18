ee0c6f398465fe753d617f01b693f7cc17b1c43f
verypluming <plumazure@gmail.com>
2017-12-19 00:06:10 +0900

    added expected in json

--- scripts/abduction_phrase.py
+++ scripts/abduction_phrase.py
@@ -121,11 +121,11 @@ def try_phrase_abduction(coq_script, previous_axioms=set(), features={}, expecte
     if is_theorem_almost_defined(output_lines):
         if expected == target:
             #positive label
-            features_log = {"features": features, "validity": 1.0, "gold": target}
+            features_log = {"features": features, "validity": 1.0, "gold": target, "expected": expected}
             print(json.dumps(features_log), file=sys.stderr)
         else:
             #negative label
-            features_log = {"features": features, "validity": 0.0, "gold": target}
+            features_log = {"features": features, "validity": 0.0, "gold": target, "expected": expected}
             print(json.dumps(features_log), file=sys.stderr)
         return expected, [new_coq_script], previous_axioms, features
     premise_lines = get_premise_lines(output_lines)
@@ -148,11 +148,11 @@ def try_phrase_abduction(coq_script, previous_axioms=set(), features={}, expecte
     inference_result_str = expected if is_theorem_almost_defined(output_lines) else 'unknown'
     if inference_result_str == target:
         #positive label
-        features_log = {"features": features, "validity": 1.0, "gold": target}
+        features_log = {"features": features, "validity": 1.0, "gold": target, "expected": expected}
         print(json.dumps(features_log), file=sys.stderr)
     else:
         #negative label
-        features_log = {"features": features, "validity": 0.0, "gold": target}
+        features_log = {"features": features, "validity": 0.0, "gold": target, "expected": expected}
         print(json.dumps(features_log), file=sys.stderr)
     return inference_result_str, [new_coq_script], axioms, features
 
