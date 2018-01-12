36b883cab07d52ec94570e27a261bdd46075831c
verypluming <plumazure@gmail.com>
2018-01-12 22:02:06 +0900

    added siamese-mlp-model temporarily

--- scripts/phrase_classifier.py
+++ scripts/phrase_classifier.py
@@ -40,6 +40,180 @@ def crossvalidation(clf, X_train, y_train):
     scores = cross_val_score(clf, X_train, y_train, cv=10)
     return scores.mean(), scores.std()
 
+def text_to_word_list(text):
+    #preprocess and convert texts to a list of words
+    text = str(text)
+    text = text.lower()
+
+    # Clean the text
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+
+    text = text.split()
+
+    return text
+
+def siamese_mlp_model():
+    from nltk.corpus import stopwords
+    from keras.preprocessing.sequence import pad_sequences
+    from keras.models import Model
+    from keras.layers import Input, Embedding, LSTM, Merge, Bidirectional, Dense, Concatenate
+    import keras.backend as K
+    from keras.optimizers import Adadelta
+    from keras.callbacks import ModelCheckpoint
+    # concatenate sentence vector obtained from siamese-model and premise-subgoal similarity vector obtained from proof
+    # Prepare embedding
+    EMBEDDING_FILE = './GoogleNews-vectors-negative300.bin'
+
+    # Load training and test set
+    inputfiles = glob.glob("./plain/sick_*.txt")
+    half = int(len(inputfiles)/2)
+    inputfile_sentences = []
+    for inputfile in inputfiles:
+        filename = re.search("(sick_[a-z]*_[0-9]*).", inputfile).group(1)
+        f = open(inputfile, "r")
+        s = f.readlines()
+        f.close()
+        g = open("./plain2/"+filename+".answer", "r")
+        sim = g.readlines()[0]
+        g.close()
+        inputfile_sentences.append([s[0].strip(), s[1], sim])
+    df = pd.DataFrame(inputfile_sentences, columns=['sentence1', 'sentence2', 'sim'])
+    train_df = df[:half]
+    test_df = df[half:]
+
+    stops = set(stopwords.words('english'))
+    vocabulary = dict()
+    inverse_vocabulary = ['<unk>']  # '<unk>' will never be used, it is only a placeholder for the [0, 0, ....0] embedding
+    word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
+    sentences_cols = ['sentence1', 'sentence2']
+
+    # Iterate over the sentences only of both training and test datasets
+    for dataset in [train_df, test_df]:
+        for index, row in dataset.iterrows():
+
+            # Iterate through the text of both sentences of the row
+            for sentence in sentences_cols:
+                q2n = []  # q2n -> sentence numbers representation
+                for word in text_to_word_list(row[sentence]):
+                    # Check for unwanted words
+                    if word in stops and word not in word2vec.vocab:
+                        continue
+
+                    if word not in vocabulary:
+                        vocabulary[word] = len(inverse_vocabulary)
+                        q2n.append(len(inverse_vocabulary))
+                        inverse_vocabulary.append(word)
+                    else:
+                        q2n.append(vocabulary[word])
+                # Replace sentence as word to sentence as number representation
+                dataset.set_value(index, sentence, q2n)
+    embedding_dim = 300
+    embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)
+    embeddings[0] = 0  # So that the padding will be ignored
+
+    # Build the embedding matrix
+    for word, index in vocabulary.items():
+        if word in word2vec.vocab:
+            embeddings[index] = word2vec.word_vec(word)
+
+    del word2vec
+
+    max_seq_length = max(train_df.sentence1.map(lambda x: len(x)).max(),
+                     train_df.sentence2.map(lambda x: len(x)).max(),
+                     test_df.sentence1.map(lambda x: len(x)).max(),
+                     test_df.sentence2.map(lambda x: len(x)).max())
+
+    # Split to train validation
+    validation_size = 2000
+    training_size = len(train_df) - validation_size
+
+    X = train_df[sentences_cols]
+    Y = train_df['sim']
+
+    X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size)
+
+    # Split to dicts
+    X_train = {'left': X_train.sentence1, 'right': X_train.sentence2}
+    X_validation = {'left': X_validation.sentence1, 'right': X_validation.sentence2}
+    X_test = {'left': test_df.sentence1, 'right': test_df.sentence2}
+
+    # Convert labels to their numpy representations
+    Y_train = Y_train.values
+    Y_validation = Y_validation.values
+
+    # Zero padding
+    for dataset, side in itertools.product([X_train, X_validation], ['left', 'right']):
+        dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+
+    # Make sure everything is ok
+    assert X_train['left'].shape == X_train['right'].shape
+    assert len(X_train['left']) == len(Y_train)
+
+    n_hidden = 50
+    gradient_clipping_norm = 1.25
+    batch_size = 64
+    n_epoch = 25
+
+    left_input = Input(shape=(max_seq_length,), dtype='int32')
+    right_input = Input(shape=(max_seq_length,), dtype='int32')
+
+    embedding_layer = Embedding(len(embeddings), embedding_dim, weights=[embeddings], input_length=max_seq_length, trainable=False)
+
+    # Embedded version of the inputs
+    encoded_left = embedding_layer(left_input)
+    encoded_right = embedding_layer(right_input)
+
+    # Since this is a siamese network, both sides share the same LSTM
+    shared_lstm = Bidirectional(LSTM(n_hidden))
+
+    left_output = shared_lstm(encoded_left)
+    right_output = shared_lstm(encoded_right)
+
+    th_output = Concatenate()([left_output, right_output])
+
+    sim_input = Input(shape=(28,))
+    sim_input1 = Dense(100, activation='relu')(sim_input)
+    sim_input2 = Dense(28, activation='relu')(sim_input1)
+    merge_input = Concatenate()([th_output, sim_input2])
+
+    predictions = Dense(1, init='uniform', activation='sigmoid')(merge_input)
+    model = Model([left_input, right_input, sim_input], outputs=predictions)
+
+    # Adadelta optimizer, with gradient clipping by norm
+    optimizer = Adadelta(clipnorm=gradient_clipping_norm)
+    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    return model
+
+
 def base_model(activation="relu", optimizer="adam", out_dim=20):
     base_model = Sequential()
     base_model.add(Dense(input_dim=14, units=1))
